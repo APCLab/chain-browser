891d78f8084cb6ab1d323248eaa6430905bca9bb
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-10-31 12:07:43 +0800

    [app] rename import name

--- apps.py
+++ apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class BrowserConfig(AppConfig):
-    name = 'browser'
+class ChainBrowserConfig(AppConfig):
+    name = 'chain_browser'
--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from browser import views
+from chain_browser import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
